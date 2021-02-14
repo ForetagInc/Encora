00be0f783deb5b2377161902d79f42bdb2c65ed4
itzezc <chirub@foretag.co>
2021-02-14 02:29:25 +0000

    [icon] Component

---
+++ components/Icon/source/icon.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as Font from 'expo-font';
+import { createIconSet } from '@expo/vector-icons';
+
+import { IconProps } from './icon.interface';
+
+const RemixIcon = createIconSet(require('../assets/glyphs.json'), 'RemixIcon', Font.loadAsync({ 'RemixIcon': require('remixicon/fonts/remixicon.ttf') }));
+
+export const Icon: React.FC<IconProps> = (props) => <RemixIcon { ...props } />;
+
+Icon.displayName = 'Icon';
--- components/Icon/source/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as Font from 'expo-font';
-import { createIconSet } from '@expo/vector-icons';
-
-const RemixIcon = createIconSet(require('./assets/glpyhs.json'), 'RemixIcon', Font.loadAsync('remixicon/fonts/remixicon.ttf'));
-
-export const Icon: React.FC = (props) => <RemixIcon { ...props } />;
-
-Icon.displayName = 'Icon';
